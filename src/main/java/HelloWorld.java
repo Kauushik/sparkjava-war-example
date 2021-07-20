66fd1bf59ed9ad0f211c8748c3e4a428a4e247f3
kauushik <kauushik.selvan@gmai.com>
2021-07-20 15:51:49 +0530

    modified for webhook concecpt

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "This is Kauushik World");
+		get("/hello", (req, res) -> "Kauushik World");
 	}
 }
